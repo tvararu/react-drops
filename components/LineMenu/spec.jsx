266fe022ccfcbe5d5aac175e3eb0e24a7bdb04c3
Theodor Vararu <theo@vararu.org>
2016-01-23 23:11:52 +0000

    Ramda is slightly better.

--- components/LineMenu/spec.jsx
+++ components/LineMenu/spec.jsx
@@ -1,36 +1,24 @@
-import _ from 'lodash/fp'
 import R from 'ramda'
-// import React from 'react'
+import React from 'react'
 import test from 'tape'
-// import { shallow } from 'enzyme'
-// import { LineMenu } from './index'
+import { shallow } from 'enzyme'
+import { LineMenu } from './index'
 
 const menuItems = ['Home', 'Gallery', 'Portfolio', 'Clients', 'Contact']
-// const menuItems = ['Home', 'Home']
 
-// // contains :: String -> String -> Boolean
-// const contains = _.curry((needle, haystack) => Boolean(haystack.match(RegExp(needle))))
-//
-// // allAreTrue :: [Boolean] -> Boolean
-// const allAreTrue = _.every(_.identity)
-//
-// // containsMultiple :: [String] -> String -> [Boolean]
-// const containsMultiple = _.curry((needles, haystack) => {
-//   const runAgainstHaystack = _.map((f) => f(haystack))
-//   return _.flow(_.map(contains), runAgainstHaystack, allAreTrue)(needles)
-//   // needles.map(contains)
-//   // return _.contains()
-//   // return _.contains(needles[0], haystack)
-// })
+// allAreTrue :: [Boolean] -> Boolean
+const allAreTrue = R.all(R.identity)
 
-test('<LineMenu /> displays five menu items', (assert) => {
-  // const wrapper = shallow(<LineMenu
-  //   items={ menuItems }
-  // />)
-  // assert.equal(wrapper.text(), true)
-  const testStr = 'Lorem Home ipsum'
-  // console.log(containsMultiple(menuItems)(testStr))
-  // console.log(contains(menuItems[0])(testStr))
+// contains :: [String] -> String -> Boolean
+const contains = R.curry((needles, haystack) =>
+  R.pipe(R.map(R.contains(R.__, haystack)), allAreTrue)(needles)
+)
+
+test('<LineMenu /> displays five menu items', assert => {
+  const wrapper = shallow(<LineMenu
+    items={ menuItems }
+  />)
+  assert.equal(contains(menuItems, wrapper.text()), true)
 
   assert.end()
 })
