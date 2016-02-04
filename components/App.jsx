18ca2b05aa86f03dc5d9335396d632590b02a413
Theodor Vararu <theo@vararu.org>
2016-02-04 22:23:30 +0000

    Render <LineMenu /> in main view.

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Hello } from './Hello'
+import { LineMenu } from './LineMenu'
 
 export class App extends React.Component {
   render () {
+    const menuItems = ['Home', 'Gallery', 'Portfolio', 'Clients', 'Contact']
     return <div>
       <Hello />
+      <LineMenu items={ menuItems } />
     </div>
   }
 }
